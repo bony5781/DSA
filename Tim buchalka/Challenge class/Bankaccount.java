f0b30544fa10cb9e0b179f00e4e08cd71fd957e9
bony5781 <bhowmikabhinav@gmail.com>
2023-11-21 18:01:54 +0530

    LCS

---
+++ Tim buchalka/Challenge class/Bankaccount.java
@@ -0,0 +1,61 @@
+public class Bankaccount {
+
+    private int accountNumber;
+    private int accountBalance;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(int accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addMoney(int money) {
+        this.accountBalance += money;
+    }
+
+    public void debitMoney(int money) {
+        if (this.accountBalance - money < 0) {
+            System.out.println("Not allowed!");
+            return;
+        }
+        this.accountBalance -= money;
+    }
+
+}
