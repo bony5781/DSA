937cee83a704e033c67a88cdb86adf2bcad9baa2
bony5781 <bhowmikabhinav@gmail.com>
2023-11-25 16:55:39 +0530

    Revision

---
+++ Revisionrandomizer.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Revisionrandomizer {
+
+    public static int getRandom(int[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+
+    public static void main(String[] args) {
+        int array[] = { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 2, 56, 57, 58, 59, 60, 61, 62, 63,
+                64, 65 };
+        System.out.println(getRandom(array));
+    }
+}
