c93e78aa4eb7238c730955ff3f0921db8b3563ba
bony5781 <bhowmikabhinav@gmail.com>
2023-11-26 22:23:24 +0530

    need to problem solve

---
+++ String/67 Split string into 0 and 1/Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+    public static int maxSubStr(String str) {
+        // Write your code here
+        int counter = 0;
+        int n = str.length();
+        int i = 0;
+
+        while (i < n) {
+            int zeroCount = 0;
+            int oneCount = 0;
+
+            System.out.println(i);
+            if (str.charAt(i) == 0) {
+                while (i < n && str.charAt(i) == '0') {
+                    zeroCount++;
+                    i += 1;
+                }
+
+                while (i < n && str.charAt(i) == '1') {
+                    oneCount++;
+                    i += 1;
+                }
+            }else{
+                 while (i < n && str.charAt(i) == '1') {
+                    oneCount++;
+                    i += 1;
+                }
+
+                while (i < n && str.charAt(i) == '0') {
+                    zeroCount++;
+                    i += 1;
+                }
+            }
+
+            if (zeroCount == oneCount) {
+                counter++;
+            }
+
+        }
+
+        return counter;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubStr("0101"));
+    }
+}
