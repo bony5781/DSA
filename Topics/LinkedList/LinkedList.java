0da3e9390b49c5a2e643ead80a731a1fc5369c17
bony5781 <bhowmikabhinav@gmail.com>
2023-12-01 13:16:37 +0530

    LL

---
+++ Topics/LinkedList/LinkedList.java
@@ -0,0 +1,61 @@
+public class LinkedList {
+
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        if (head == null)
+            System.out.println("List is empty");
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.print("NULL");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        list.addLast("how");
+        list.addLast("wtf");
+        list.printList();
+    }
+}
