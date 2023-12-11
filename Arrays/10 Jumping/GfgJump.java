e417c3e4411565341d21c95aadb68bb75ad229f2
bony5781 <bhowmikabhinav@gmail.com>
2023-12-11 13:10:15 +0530

    Max Rectangle

--- Arrays/10 Jumping/GfgJump.java
+++ Arrays/10 Jumping/GfgJump.java
@@ -6,6 +6,7 @@ public class GfgJump {
         
         for(int i = 0; i < arr.length-1; i++){
             des = Math.max(des, arr[i] + i);
+            System.err.println(des);
             if(pos == i){
                 pos = des;
                 jump++;
@@ -15,5 +16,10 @@ public class GfgJump {
         if(pos >= arr.length - 1){
             return jump;
         }else return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+        System.out.println(minJumps(arr));
     }
 }
