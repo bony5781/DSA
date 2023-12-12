1a89f51d35af00dd143ff8c1fdca777c867c49e0
bony5781 <bhowmikabhinav@gmail.com>
2023-12-12 10:38:16 +0530

    Tim

---
+++ Tim buchalka/40 Polymorphism/Mitsubishi.java
@@ -0,0 +1,21 @@
+public class Mitsubishi extends Car {
+    // write code here
+    public Mitsubishi(int cylinders, String name){
+        super(cylinders, name);
+    }
+    
+    @Override
+    public String startEngine(){
+        return ("car's engine is starting of class " + this.getClass().getSimpleName());
+    }
+    
+    @Override
+    public String accelerate(){
+        return ("car is accelarating of class " + this.getClass().getSimpleName());
+    }
+    
+    @Override
+    public String brake(){
+        return ("car is braking of class " + this.getClass().getSimpleName());
+    }
+}
