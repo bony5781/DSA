a625dab7684d2e7b9c267b8471fa95c04faa954e
bony5781 <bhowmikabhinav@gmail.com>
2023-12-24 22:43:43 +0530

    book

--- Revision/src/Main.java
+++ Revision/src/Main.java
@@ -1,26 +1,67 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
 
-    //21-12-23
-    public static int linearSearch(int arr[], int n,int target){
+    //All occurrences of linear Search
+    public int[] linearSearchImproved(int[] arr, int n, int target){
+        ArrayList<Integer> arr2 = new ArrayList<>();
         for(int i = 0; i < n; i++){
             if(arr[i] == target){
-                return i;
+                arr2.add(i);
             }
         }
-        return -1;
+
+        n = arr2.size();
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++){
+            ans[i] =arr2.get(i);
+        }
+
+        return ans;
     }
 
+//    23-12-23
+//    public int binarySearch(int arr[], int n, int target){
+//        int low = 0;
+//        int high = n - 1;
+//        int mid = low + (high - low) / 2;
+//        while(low <= high){
+//            mid = low + (high - low) / 2;
+//            if(arr[mid] == target){
+//                return mid;
+//            }else if(arr[mid] > target){
+//                high = mid - 1;
+//            }else{
+//                low = mid + 1;
+//            }
+//        }
+//
+//        return -1;
+//    }
+
+    //21-12-23
+//    public static int linearSearch(int arr[], int n,int target){
+//        for(int i = 0; i < n; i++){
+//            if(arr[i] == target){
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+
     public static void main(String[] args) {
-        int arr[] = {7, 2, 9,  1, 6, 9, 9};
+        int[] arr = {1, 2, 6, 3, 4, 5, 6, 6, 7, 8, 9};
         int n = arr.length;
         Main rv = new Main();
-        int res = linearSearch(arr, n, 6);
-        if(res != -1){
-            System.out.printf("Element first found at index %d.", res);
+        int[] res = rv.linearSearchImproved(arr, n, 6);
+        int n1 = res.length;
+        if(n1 != 0){
+            System.out.print("Element found at index");
+            for (int re : res) {
+                System.out.printf(" %d, ", re);
+            }
         }else{
-            System.out.printf("Element not found", res);
+            System.out.print("Element not found");
         }
 
     }
