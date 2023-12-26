96ef260d64e04d6b04eaf46fae15f717a0217a25
bony5781 <bhowmikabhinav@gmail.com>
2023-12-26 20:32:25 +0530

    Revision

--- Revision/src/revision/com/Main.java
+++ Revision/src/revision/com/Main.java
@@ -5,30 +5,53 @@ import java.util.Arrays;
 
 public class Main {
 
-    //Bubble Sort 25-12-23
-    public void BubbleSort(int[] arr, int n){
-
-        boolean swapped;
-        for(int i = 0; i < n - 1; i++){
-            swapped = false;
-            for(int j = 0; j < n - i - 1; j++){
-                if(arr[j] > arr[j + 1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            if(swapped == false) break;
-        }
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
+    //DSA Babbar sheet 09 26-12-23
+    public void sort012(int[] arr, int n) {
+        int low = 0;
+        int high = n - 1;
+        int i = 0;
+        while (i <= high) {
+            if(arr[i] == 0){
+                swap(arr, low, i);
+                low ++;  i++;
+            }else if(arr[i] == 1){
+                i ++;
+            }else if(arr[i] == 2) {
+                swap(arr, high, i);
+                high--;
+            }
+        }
+    }
+    //Bubble Sort 25-12-23
+//    public void BubbleSort(int[] arr, int n){
+//
+//        boolean swapped;
+//        for(int i = 0; i < n - 1; i++){
+//            swapped = false;
+//            for(int j = 0; j < n - i - 1; j++){
+//                if(arr[j] > arr[j + 1]){
+//                    int temp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = temp;
+//                    swapped = true;
+//                }
+//            }
+//            if(swapped == false) break;
+//        }
+//    }
+
     public static void main(String[] args) {
-        int[] arr = {2, 1, 5, 3, 7, 1, 99, 4};
+        int[] arr = {0, 1, 0};
         int n = arr.length;
         System.out.println("Array before sorting : " + Arrays.toString(arr));
         Main rv = new Main();
-        rv.BubbleSort(arr, n);
+        rv.sort012(arr, n);
         System.out.println("Array after sorting : " + Arrays.toString(arr));
     }
 }
