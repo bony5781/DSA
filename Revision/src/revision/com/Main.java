500dfb6f83322825c77b21ead91d669ef929f172
bony5781 <bhowmikabhinav@gmail.com>
2023-12-28 20:00:48 +0530

    Revision

--- Revision/src/revision/com/Main.java
+++ Revision/src/revision/com/Main.java
@@ -5,49 +5,64 @@ import java.util.Arrays;
 
 public class Main {
 
+    //Kadane's Algorithm 28-02-23
+    public int kadan(int[] arr, int n){
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for(int i = 0; i < n; i++){
+            currSum += arr[i];
+
+            maxSum = Math.max(currSum, maxSum);
+            if(currSum < 0) currSum = 0;
+        }
+
+        return maxSum;
+    }
+
 //    public void swap(int[] arr, int i, int j){
 //        int temp = arr[i];
 //        arr[i] = arr[j];
 //        arr[j] = temp;
 //    }
 
-    public void merge(int[] arr, int low, int mid, int high) {
-        int[] arr2 = new int[high - low + 1];
-        int i = 0, j = mid + 1;
-        int k = 0;
-        while (i <= mid && j <= high) {
-            if (arr[i] > arr[j]) {
-                arr2[k++] = arr[j++];
-            } else {
-                arr2[k++] = arr[i++];
-            }
-        }
-
-        while (i <= mid) {
-            arr2[k++] = arr[i++];
-        }
-
-        while (j <= high) {
-            arr2[k++] = arr[j++];
-        }
-
-        for (int m = low, l = 0; m <= arr2.length; l++, m++) {
-            arr[l] = arr2[m];
-        }
-
-    }
+//    public void merge(int[] arr, int low, int mid, int high) {
+//        int[] arr2 = new int[high - low + 1];
+//        int i = 0, j = mid + 1;
+//        int k = 0;
+//        while (i <= mid && j <= high) {
+//            if (arr[i] > arr[j]) {
+//                arr2[k++] = arr[j++];
+//            } else {
+//                arr2[k++] = arr[i++];
+//            }
+//        }
+//
+//        while (i <= mid) {
+//            arr2[k++] = arr[i++];
+//        }
+//
+//        while (j <= high) {
+//            arr2[k++] = arr[j++];
+//        }
+//
+//        for (int m = low, l = 0; m <= arr2.length; l++, m++) {
+//            arr[l] = arr2[m];
+//        }
+//
+//    }
 
     //Merge Sort 27-12-23
-    public void mergeSort(int[] arr, int low, int high) {
-
-        if (low >= high) return;
-
-        int mid = low + (high - low) / 2;
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
-        merge(arr, low, mid, high);
-
-    }
+//    public void mergeSort(int[] arr, int low, int high) {
+//
+//        if (low >= high) return;
+//
+//        int mid = low + (high - low) / 2;
+//        mergeSort(arr, low, mid);
+//        mergeSort(arr, mid + 1, high);
+//        merge(arr, low, mid, high);
+//
+//    }
 
     //DSA Babbar sheet 09 26-12-23
     //Bubble Sort 25-12-23
@@ -69,11 +84,9 @@ public class Main {
 //    }
 
     public static void main(String[] args) {
-        int[] arr = {0, 1, 0};
-        int n = arr.length - 1;
-        System.out.println("Array before sorting : " + Arrays.toString(arr));
+        int[] arr = {-10, -2, -3, -4};
+        int n = arr.length ;
         Main rv = new Main();
-        rv.mergeSort(arr, 0, n);
-        System.out.println("Array after sorting : " + Arrays.toString(arr));
+        System.out.println("Maximum subarray : " + rv.kadan(arr, n));
     }
 }
