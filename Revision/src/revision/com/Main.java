09dc6410ed44e4629f59ba96a1a5c114edfca7c0
bony5781 <bhowmikabhinav@gmail.com>
2023-12-29 11:30:16 +0530

    Revision

--- Revision/src/revision/com/Main.java
+++ Revision/src/revision/com/Main.java
@@ -5,26 +5,52 @@ import java.util.Arrays;
 
 public class Main {
 
-    //Kadane's Algorithm 28-02-23
-    public int kadan(int[] arr, int n){
-        int maxSum = Integer.MIN_VALUE;
-        int currSum = 0;
-
-        for(int i = 0; i < n; i++){
-            currSum += arr[i];
-
-            maxSum = Math.max(currSum, maxSum);
-            if(currSum < 0) currSum = 0;
-        }
-
-        return maxSum;
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-//    public void swap(int[] arr, int i, int j){
-//        int temp = arr[i];
-//        arr[i] = arr[j];
-//        arr[j] = temp;
+    // Quick Sort 29-12-23
+    public int partition(int[] arr, int low, int high) {
+        int i = low - 1;
+        int pivot = arr[high];
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                 i++;
+                 swap(arr, i, j);
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    }
+
+    public void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pvtidx = partition(arr, low, high);
+            quickSort(arr, low, pvtidx - 1);
+            quickSort(arr, pvtidx + 1, high);
+        }
+    }
+
+    //Kadane's Algorithm 28-02-23
+//    public int kadan(int[] arr, int n){
+//        int maxSum = Integer.MIN_VALUE;
+//        int currSum = 0;
+//
+//        for(int i = 0; i < n; i++){
+//            currSum += arr[i];
+//
+//            maxSum = Math.max(currSum, maxSum);
+//            if(currSum < 0) currSum = 0;
+//        }
+//
+//        return maxSum;
 //    }
+
 
 //    public void merge(int[] arr, int low, int mid, int high) {
 //        int[] arr2 = new int[high - low + 1];
@@ -85,8 +111,10 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {-10, -2, -3, -4};
-        int n = arr.length ;
+        int n = arr.length;
         Main rv = new Main();
-        System.out.println("Maximum subarray : " + rv.kadan(arr, n));
+        System.out.println("Array before sorting : " + Arrays.toString(arr));
+        rv.quickSort(arr, 0, n - 1);
+        System.out.println("Array before sorting : " + Arrays.toString(arr));
     }
 }
