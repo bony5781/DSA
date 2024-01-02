91502d2411969018edd07d49881b0a6f9e4f968a
bony5781 <bhowmikabhinav@gmail.com>
2024-01-02 14:23:31 +0530

    Revision

--- Revision/src/revision/com/Main.java
+++ Revision/src/revision/com/Main.java
@@ -1,120 +1,28 @@
 package revision.com;
 
-
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
 
-    public void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    // Quick Sort 29-12-23
-    public int partition(int[] arr, int low, int high) {
-        int i = low - 1;
-        int pivot = arr[high];
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                 i++;
-                 swap(arr, i, j);
-            }
+    //Union of 2 arrays
+    public int doUnion(int[] a, int n, int[] b, int m){
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            hmap.put(a[i],i);
         }
-        i++;
-        int temp = arr[i];
-        arr[i] = pivot;
-        arr[high] = temp;
-        return i;
-    }
-
-    public void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pvtidx = partition(arr, low, high);
-            quickSort(arr, low, pvtidx - 1);
-            quickSort(arr, pvtidx + 1, high);
+        for(int i = 0; i < m; i++){
+            hmap.put(b[i],i);
         }
+
+        return hmap.size();
     }
-
-    //Kadane's Algorithm 28-02-23
-//    public int kadan(int[] arr, int n){
-//        int maxSum = Integer.MIN_VALUE;
-//        int currSum = 0;
-//
-//        for(int i = 0; i < n; i++){
-//            currSum += arr[i];
-//
-//            maxSum = Math.max(currSum, maxSum);
-//            if(currSum < 0) currSum = 0;
-//        }
-//
-//        return maxSum;
-//    }
-
-
-//    public void merge(int[] arr, int low, int mid, int high) {
-//        int[] arr2 = new int[high - low + 1];
-//        int i = 0, j = mid + 1;
-//        int k = 0;
-//        while (i <= mid && j <= high) {
-//            if (arr[i] > arr[j]) {
-//                arr2[k++] = arr[j++];
-//            } else {
-//                arr2[k++] = arr[i++];
-//            }
-//        }
-//
-//        while (i <= mid) {
-//            arr2[k++] = arr[i++];
-//        }
-//
-//        while (j <= high) {
-//            arr2[k++] = arr[j++];
-//        }
-//
-//        for (int m = low, l = 0; m <= arr2.length; l++, m++) {
-//            arr[l] = arr2[m];
-//        }
-//
-//    }
-
-    //Merge Sort 27-12-23
-//    public void mergeSort(int[] arr, int low, int high) {
-//
-//        if (low >= high) return;
-//
-//        int mid = low + (high - low) / 2;
-//        mergeSort(arr, low, mid);
-//        mergeSort(arr, mid + 1, high);
-//        merge(arr, low, mid, high);
-//
-//    }
-
-    //DSA Babbar sheet 09 26-12-23
-    //Bubble Sort 25-12-23
-//    public void BubbleSort(int[] arr, int n){
-//
-//        boolean swapped;
-//        for(int i = 0; i < n - 1; i++){
-//            swapped = false;
-//            for(int j = 0; j < n - i - 1; j++){
-//                if(arr[j] > arr[j + 1]){
-//                    int temp = arr[j];
-//                    arr[j] = arr[j + 1];
-//                    arr[j + 1] = temp;
-//                    swapped = true;
-//                }
-//            }
-//            if(swapped == false) break;
-//        }
-//    }
 
     public static void main(String[] args) {
-        int[] arr = {-10, -2, -3, -4};
-        int n = arr.length;
-        Main rv = new Main();
-        System.out.println("Array before sorting : " + Arrays.toString(arr));
-        rv.quickSort(arr, 0, n - 1);
-        System.out.println("Array before sorting : " + Arrays.toString(arr));
+        int[] arr1 = {-10, -2, -3, -4};
+        int[] arr2 = {7, 55, 6, -4};
+        int n = arr1.length;
+        int m = arr2.length;
+        Main main = new Main();
+        System.out.println("Answer : " + main.doUnion(arr1, n, arr2, m));
     }
 }
