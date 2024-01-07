bc0d218c5732bb494cd5c6ca3027231fcbd5aaf2
bony5781 <bhowmikabhinav@gmail.com>
2024-01-07 11:22:43 +0530

    25th brute force method working

--- Arrays/25 Rearrange arrays/Solution.java
+++ Arrays/25 Rearrange arrays/Solution.java
@@ -16,6 +16,7 @@ public class Solution {
                 q++;
             }
         }
+
         p = 0; q = 0;
         int i = 0;
         while(hmap1.size() > 0 && hmap2.size() > 0){
@@ -35,18 +36,20 @@ public class Solution {
             arr[i] = hmap1.get(p);
             hmap1.remove(p);
             p++;
+            i++;
         }
 
         while(hmap2.size() > 0){
             arr[i] = hmap2.get(q);
             hmap2.remove(q);
             q++;
+            i++;
         }
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = new int[]{1, 2, 3, -4, -1, 4};
+        int[] arr = new int[]{1, 2, 3, -4, -1, 9};
         int n = arr.length;
         System.out.println("Arrays before rearranging : " + Arrays.toString(arr));
         s.rearrange(arr, n);
