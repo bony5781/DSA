1b7ea9a7e04b6ced2c642ef22575dbb4f6abe7eb
bony5781 <bhowmikabhinav@gmail.com>
2024-01-12 11:16:48 +0530

    DSA Revision

--- Revision/src/revision/com/Main.java
+++ Revision/src/revision/com/Main.java
@@ -5,18 +5,34 @@ import java.util.HashMap;
 
 public class Main {
 
+    // Revision 12/10/24
+    // public boolean searchMatrix(int[][] matrix, int target) {
+    //     int m = matrix[0].length;
+    //     int n = matrix.length;
+    //     for (int i = 0; i < n; i++) {
+    //         if (target >= matrix[i][0] && target <= matrix[i][m - 1]) {
+    //             for (int j = 0; j < m; j++) {
+    //                 if (target == matrix[i][j]) {
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
     // Revision 11/10/24
-    public void insertionSort(int[] arr, int n) {
-        for (int i = 1; i < n; i++) {
-            int curr = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] >= curr) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = curr;
-        }
-    }
+    // public void insertionSort(int[] arr, int n) {
+    // for (int i = 1; i < n; i++) {
+    // int curr = arr[i];
+    // int j = i - 1;
+    // while (j >= 0 && arr[j] >= curr) {
+    // arr[j + 1] = arr[j];
+    // j--;
+    // }
+    // arr[j + 1] = curr;
+    // }
+    // }
 
     // Revision 10/01/24
     // public int getPairsCount(int[] arr, int n, int k) {
