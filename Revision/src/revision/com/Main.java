f65b7a8ac13aed6e826a8f76294fa9168ce38c93
bony5781 <bhowmikabhinav@gmail.com>
2024-01-17 11:54:48 +0530

    Revision

--- Revision/src/revision/com/Main.java
+++ Revision/src/revision/com/Main.java
@@ -6,32 +6,105 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = { 5, 1, 7, 12, 23 };
+        int[] arr = { 5, 1, 7, 12, 23, 7, -1 };
         int n = arr.length;
         Main main = new Main();
-        System.out.println("Array before rotating = " + Arrays.toString(arr));
-        main.rotate(arr, n, 13);
-        System.out.println("Array after rotating = " + Arrays.toString(arr));
+        System.out.println("Array before sorting = " + Arrays.toString(arr));
+        main.quickSort(arr, 0, n - 1);
+        System.out.println("Array after sorting = " + Arrays.toString(arr));
     }
 
-    // Revision 16/01/24
-    public void reverse(int[] arr, int i, int j) {
-        int left = i;
-        int right = j;
-        while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++; right--;
+    // Revision 17/01/24
+    public int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low - 1;
+        for(int j = low; j < high; j++){
+            if(arr[j] < pivot){
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = arr[high];
+        arr[high] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
+    public void quickSort(int[] arr, int low, int high) {
+        if(low < high) {
+            int pvtidx = partition(arr, low, high);
+            quickSort(arr, low, pvtidx - 1);
+            quickSort(arr, pvtidx + 1, high);
         }
     }
 
-    public void rotate(int arr[], int n, int k) {
-        k = (k > n) ? (k % n): k;
-        reverse(arr, 0, n - 1);
-        reverse(arr, 0, k - 1);
-        reverse(arr, k, n - 1);
-    }
+    // public void merge(int[] arr, int low, int mid, int high) {
+    // int[] merged = new int[high - low + 1];
+    // int i = low, j = mid + 1, k = 0;
+
+    // while (i <= mid && j <= high) {
+    // if (arr[i] <= arr[j]) {
+    // merged[k++] = arr[i++];
+    // } else {
+    // merged[k++] = arr[j++];
+    // }
+    // }
+
+    // while (i <= mid) {
+    // merged[k++] = arr[i++];
+    // }
+
+    // while (j <= high) {
+    // merged[k++] = arr[j++];
+    // }
+
+    // for (int l = 0, m = low; l < merged.length; l++, m++) {
+    // arr[m] = merged[l];
+    // }
+    // }
+
+    // public void mergeSort(int[] arr, int low, int high) {
+    // if (low >= high)
+    // return;
+    // int mid = low + (high - low) / 2;
+    // mergeSort(arr, low, mid);
+    // mergeSort(arr, mid + 1, high);
+    // merge(arr, low, mid, high);
+    // }
+
+    // public void insertionSort(int[] arr, int n) {
+    // for (int i = 0; i < n; i++) {
+    // int curr = arr[i];
+    // int j = i - 1;
+    // while (j >= 0 && curr < arr[j]) {
+    // arr[j + 1] = arr[j];
+    // j--;
+    // }
+    // arr[j + 1] = curr;
+    // }
+    // }
+
+    // Revision 16/01/24
+    // public void reverse(int[] arr, int i, int j) {
+    // int left = i;
+    // int right = j;
+    // while (left < right) {
+    // int temp = arr[left];
+    // arr[left] = arr[right];
+    // arr[right] = temp;
+    // left++; right--;
+    // }
+    // }
+
+    // public void rotate(int arr[], int n, int k) {
+    // k = (k > n) ? (k % n): k;
+    // reverse(arr, 0, n - 1);
+    // reverse(arr, 0, k - 1);
+    // reverse(arr, k, n - 1);
+    // }
 
     // Revision 15/01/24
     // public void mergeSort(int[] arr, int low, int mid, int high) {
