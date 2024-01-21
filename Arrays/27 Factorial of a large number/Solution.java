5cac958066d0f3efd8c1030c1b869727b5735c61
bony5781 <bhowmikabhinav@gmail.com>
2024-01-21 22:56:13 +0530

    27

--- Arrays/27 Factorial of a large number/Solution.java
+++ Arrays/27 Factorial of a large number/Solution.java
@@ -3,23 +3,32 @@ import java.util.Collections;
 
 class Solution {
 
-    public ArrayList<Integer> factorial(int N){
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        int temp = 1, counter = 0, carry = 0;
+    ArrayList<Integer> factorial(int N) {
+        ArrayList<Integer> arr = new ArrayList<>();
         arr.add(1);
-        for(int i = 2; i <= N; i++){
-            temp = temp * i;
-            if(temp >= 0 && temp <= 9){
-                arr.set(counter, temp);
-            }else{
-                arr.set(counter,temp%10);
-            }
+        for (int i = 2; i <= N; i++) {
+            multiply(arr, i);
         }
+        Collections.reverse(arr);
         return arr;
+    }
+
+    void multiply(ArrayList<Integer> arr, int n) {
+        int carry = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            int p = arr.get(i) * n + carry;
+            arr.set(i, p % 10);
+            carry = p / 10;
+        }
+        while (carry != 0) {
+            arr.add(carry % 10);
+            carry /= 10;
+        }
+        System.out.println(arr);
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.factorial(5));
+        System.out.println(s.factorial(6));
     }
 }
