d8771f31ab654049f55f7c6b3ce973bf2defd3a8
bony5781 <bhowmikabhinav@gmail.com>
2024-02-03 18:22:10 +0530

    2

---
+++ Leetcode/2/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+        ListNode currNode = listNode;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = 0;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            currNode.next = node;
+            currNode = node;
+        }
+        return listNode.next;
+    }
+}
