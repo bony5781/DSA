77f48d958cf6e9b25ec4f3b6177b2a95383d1bd4
Abhinav Bhowmik <bhowmikabhinav@gmail.com>
2024-03-24 17:05:50 +0530

    Revision

---
+++ Leetcode/Leetcode Revision/Revision.py
@@ -0,0 +1,10 @@
+#24-03-24
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
