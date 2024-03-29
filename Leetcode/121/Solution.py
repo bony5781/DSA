73eec5eae9ea5c9b402c1fdb8b8bbe85969971e8
Abhinav Bhowmik <bhowmikabhinav@gmail.com>
2024-03-29 21:21:37 +0530

    121

---
+++ Leetcode/121/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0  
+        low = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] - low > profit:
+                profit = prices[i] - low
+            if prices[i] < low:
+                low = prices[i]
+        return profit
