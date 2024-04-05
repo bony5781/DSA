500d66a32440cb265c42622d79d3d59a83d180ea
Abhinav Bhowmik <bhowmikabhinav@gmail.com>
2024-04-05 19:20:50 +0530

    Revision

--- Leetcode/Leetcode Revision/Revision.py
+++ Leetcode/Leetcode Revision/Revision.py
@@ -1,3 +1,24 @@
+#5-04-2024
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows  = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        square  = collections.defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                if (board[i][j] in rows[i] or 
+                    board[i][j] in cols[j] or
+                    board[i][j] in square[(i // 3, j // 3)]):
+                    return False
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                square[(i // 3, j // 3)].add(board[i][j])
+
+        return True
+
 #3-04-2024
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
