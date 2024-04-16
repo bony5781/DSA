01819ae95f384d4155fa1d3641f42463e1b1f7ec
Abhinav Bhowmik <bhowmikabhinav@gmail.com>
2024-04-16 19:00:59 +0530

    Revision

--- Leetcode/Leetcode Revision/Revision.py
+++ Leetcode/Leetcode Revision/Revision.py
@@ -1,3 +1,30 @@
+#16-04-2024
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        answer = []
+        for i in range(len(nums) - 2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    triplet = [nums[i], nums[l], nums[r]]
+                    answer.append(triplet)
+                    while l < r and nums[l] == triplet[1]:
+                        l += 1
+                    while l < r and nums[r] == triplet[2]:
+                        r -= 1
+        return answer
+
 #15-04-2024
 class Solution:
     def isPalindrome(self, s: str) -> bool:
