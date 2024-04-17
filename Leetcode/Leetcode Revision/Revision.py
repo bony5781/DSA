91ff67e2b91ad7edac23a84f26e128ad521e66e8
Abhinav Bhowmik <bhowmikabhinav@gmail.com>
2024-04-17 20:14:39 +0530

    Revision

--- Leetcode/Leetcode Revision/Revision.py
+++ Leetcode/Leetcode Revision/Revision.py
@@ -1,3 +1,30 @@
+#17-04-2024
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                target = nums[i] + nums[l] + nums[r]
+                if target > 0:
+                    r -= 1
+                elif target < 0:
+                    l += 1
+                else:
+                    triplet = [nums[i], nums[l], nums[r]]
+                    ans.append(triplet)
+                    while l < r and nums[l] == triplet[1]:
+                        l += 1
+                    while l < r and nums[r] == triplet[2]:
+                        r -= 1
+        
+        return ans
+
 #16-04-2024
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
