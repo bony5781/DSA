c0b661ce24477970fe2cd5ce164fc0a92dd3101a
Abhinav Bhowmik <bhowmikabhinav@gmail.com>
2024-04-18 19:44:54 +0530

    Revision

--- Leetcode/Leetcode Revision/Revision.py
+++ Leetcode/Leetcode Revision/Revision.py
@@ -1,3 +1,21 @@
+#18-04-2024
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        res  = 0
+
+        while l < r:
+            temp = min(height[l], height[r]) * ( r - l)
+            res = max(temp, res)
+
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+
+        return res
+
 #17-04-2024
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
