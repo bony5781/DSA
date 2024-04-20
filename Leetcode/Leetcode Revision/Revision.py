a3798f1843fdb9dd57a6a5cf0b34b957b282c4f2
Abhinav Bhowmik <bhowmikabhinav@gmail.com>
2024-04-20 17:42:35 +0530

    Revision

--- Leetcode/Leetcode Revision/Revision.py
+++ Leetcode/Leetcode Revision/Revision.py
@@ -1,3 +1,19 @@
+#20-04-2024
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxProfit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(maxProfit, profit) 
+            else:
+                l = r       
+            r += 1
+        
+        return maxProfit
+
 #19-04-2024
 class Solution:
     def trap(self, height: List[int]) -> int:
