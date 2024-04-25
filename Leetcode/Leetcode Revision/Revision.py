4563dae9ad1b68e596c2aecc5adc6dccb88af8d1
bony5781 <bhowmikabhinav@gmail.com>
2024-04-25 12:35:35 +0530

    Revision

--- Leetcode/Leetcode Revision/Revision.py
+++ Leetcode/Leetcode Revision/Revision.py
@@ -1,3 +1,35 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s1 = set(nums)
+        ans = 0
+
+        for x in nums:
+            if x - 1 not in s1:
+                temp = 0
+                while x + temp in s1:
+                    temp += 1
+                ans = max(ans, temp)
+        
+        return ans
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(list)
+        cols = defaultdict(list)
+        square = defaultdict(list)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in square[i//3,j//3]:
+                    return False
+                rows[i].append(board[i][j])
+                cols[j].append(board[i][j])
+                square[i//3,j//3].append(board[i][j])
+        
+        return True
+    
 #24-04-2024
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
