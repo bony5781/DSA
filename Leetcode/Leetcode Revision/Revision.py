d884c49918fe101d3365f206a0c401a55c9dc8e9
bony5781 <bhowmikabhinav@gmail.com>
2024-04-29 19:10:34 +0530

    Revision

--- Leetcode/Leetcode Revision/Revision.py
+++ Leetcode/Leetcode Revision/Revision.py
@@ -1,3 +1,19 @@
+#29-04-2024
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l , r = 0, 1
+        maxP  = 0
+
+        while r < len(prices):
+            temp = prices[r] - prices[l]
+            if temp > 0:
+                maxP = max(temp, maxP)
+            else:
+                l = r
+            r += 1
+        
+        return maxP
+
 #26-04-2024
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
