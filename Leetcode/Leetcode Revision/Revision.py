b614a7c864b48e2dadadc4af8c8c61e99e270feb
bony5781 <bhowmikabhinav@gmail.com>
2024-05-08 19:18:20 +0530

    Revision

--- Leetcode/Leetcode Revision/Revision.py
+++ Leetcode/Leetcode Revision/Revision.py
@@ -1,3 +1,23 @@
+#08-05-2024
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        ans = 0
+        leftMax, rightMax= height[l], height[r]
+
+        while l < r:
+
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                ans += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                ans += rightMax - height[r]
+            
+        return ans
+
 #07-05-2024
 class Solution:
     def maxArea(self, height: List[int]) -> int:
