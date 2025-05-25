40f2ff0556898a76fca44ca81e47ef5075ecba15
bony5781 <bhowmikabhinav@gmail.com>
2025-05-25 21:12:31 +0530

    Revision

--- Leetcode/Leetcode Revision/Solution.java
+++ Leetcode/Leetcode Revision/Solution.java
@@ -1,3 +1,19 @@
+//25-05-2025
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+}
+
+
 //24-05-25
 class Solution {
     public int[] twoSum(int[] nums, int target) {
