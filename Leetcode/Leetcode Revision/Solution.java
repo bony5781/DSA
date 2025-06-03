76da38270cff2d109bc546ec6fa1d9a9b6bcfd30
bony5781 <bhowmikabhinav@gmail.com>
2025-06-03 20:26:10 +0530

    Revision

--- Leetcode/Leetcode Revision/Solution.java
+++ Leetcode/Leetcode Revision/Solution.java
@@ -1,3 +1,19 @@
+//3-06-25
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = nums[0];
+
+        for(int num: nums){
+            if(count == 0){
+                candidate = num;
+            }
+            count = (candidate == num) ? count +  1 : count - 1;
+        }
+        return candidate;
+    }
+}
+
 //02-06-25
 class Solution {
 
