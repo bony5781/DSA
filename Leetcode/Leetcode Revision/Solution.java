5f762db0f6747cc64dd6460093c9afb1b602e474
bony5781 <bhowmikabhinav@gmail.com>
2025-06-12 21:27:51 +0530

    Revision

--- Leetcode/Leetcode Revision/Solution.java
+++ Leetcode/Leetcode Revision/Solution.java
@@ -1,3 +1,21 @@
+//12-06-25
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left= 0, right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+
 //8-06-25
 class Solution {
     public int longestConsecutive(int[] nums) {
