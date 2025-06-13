7047f9406ac4a4de9404992ac4b7374778dd1087
bony5781 <bhowmikabhinav@gmail.com>
2025-06-13 20:52:06 +0530

    Revision

--- Leetcode/Leetcode Revision/Solution.java
+++ Leetcode/Leetcode Revision/Solution.java
@@ -1,3 +1,29 @@
+//13-06-25
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int countSequence = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (sum == k) {
+                countSequence++;
+            }
+
+            if (map.containsKey(sum - k)) {
+                countSequence += map.get(sum - k);
+            }
+
+            // Always update the map with the current sum
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return countSequence;
+    }
+}
+
 //12-06-25
 class Solution {
     public int searchInsert(int[] nums, int target) {
